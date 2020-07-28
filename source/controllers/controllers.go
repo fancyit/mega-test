e0cda5a2dc2537540cbe2cf1d8edff29e4744925
Anton Bolotnov <hotpcline@gmail.com>
2020-07-28 14:46:45 +0300

    utils + controller

--- source/controllers/controllers.go
+++ source/controllers/controllers.go
@@ -1,1 +1,34 @@
-package controllers
+package controller
+
+import (
+	h "../helpers"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Pswd struct {
+	Phrase       string `json:"phrase"`
+	HexedPhrase  string
+	HashedPhrase uint64
+}
+
+var MainMethod = func(w http.ResponseWriter, r *http.Request) {
+	var pass Pswd
+	var tmpVal map[string]interface{} // проместим входящую переменную в тип мап(ключ: значение), нужный нам ключ мы знаем
+	json.NewDecoder(r.Body).Decode(&tmpVal)
+	encoder := json.NewEncoder(w)
+	encoder.SetEscapeHTML(false)
+	pass.Phrase = fmt.Sprintf("%v", tmpVal["phrase"]) //если на вход нам дали число(float64) - парсим его в строку
+	if pass.Phrase == "<nil>" {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Header().Add("Content-Type", "application/json")
+		encoder.Encode(w)
+	} else {
+		sha256bytes := sha256.Sum256([]byte(pass.Phrase)) //получаем хэш в виде массива байт
+		pass.HexedPhrase = h.B2hex(sha256bytes[:])        //хексим слайс массива
+		pass.HashedPhrase = h.Hex2int(pass.HexedPhrase)
+		encoder.Encode(&pass)
+	}
+}
--- source/helpers/utils.go
+++ source/helpers/utils.go
@@ -5,10 +5,15 @@ import (
 	"strconv"
 	"strings"
 )
+
 // массив байт в 16-ю строку
 func B2hex(b []byte) string {
 	// возвращаем шеснадцатеричную строку, убрав в ней префикс 0x, если есть
-	return strings.Replace(hex.EncodeToString(b[:]), "0x", "", -1)
+	//dst := make([]byte, hex.EncodedLen(len(b)))
+	//hex.Encode(dst, b)
+	//return fmt.Sprintf("%s", dst)
+	res := strings.Replace(hex.EncodeToString(b[:]), "0x", "", -1)
+	return res
 }
 
 // 16-я строка в uint64
@@ -17,5 +22,3 @@ func Hex2int(hexStr string) uint64 {
 	result, _ := strconv.ParseUint(hexStr, 16, 64)
 	return uint64(result)
 }
-
-//func BuildParseHash()
